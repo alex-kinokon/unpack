82779aacd69f2880753368907197330171373fae
Alex <8125011+proteriax@users.noreply.github.com>
2022-09-13 13:16:32 -0400

    Fix bug

--- src/passes/split-comma.ts
+++ src/passes/split-comma.ts
@@ -3,11 +3,16 @@ import { definePlugin } from "../utils";
 export const splitComma = definePlugin(({ types: t }) => ({
   SequenceExpression(path) {
     const { parentPath: parent, node } = path;
-    switch (parent.type) {
+    switch (parent.node.type) {
       case "ExpressionStatement":
         parent.replaceWithMultiple(node.expressions.map(e => t.expressionStatement(e)));
         break;
       case "ForStatement":
+        if (parent.node.init !== node) {
+          return;
+        }
+
+      // fallthrough
       case "IfStatement":
       case "ThrowStatement":
       case "ReturnStatement": {
